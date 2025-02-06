65a792477ddd3e9433e3a5e7507739eeafa9ca9a
kerrinna-l <thaykerrinna382@gmail.com>
2025-02-06 18:26:54 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function trocarImagem() {
+    document.getElementById('camiseta').src = 'camiseta1.jpg';
+  }
+  
+  function voltarImagem() {
+    document.getElementById('camiseta').src = 'camiseta2.jpg';
+  }
+  
